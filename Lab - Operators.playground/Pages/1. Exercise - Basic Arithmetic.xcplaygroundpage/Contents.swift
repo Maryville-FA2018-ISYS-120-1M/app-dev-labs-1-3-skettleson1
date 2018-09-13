d0a1a2110270fb4ebe4bf5b2a2b94921d0ccf3eb
Shanae Ullman <shanae.ullman@gmail.com>
2018-09-13 13:05:45 -0500

    SU edits

--- Lab - Operators.playground/Pages/1. Exercise - Basic Arithmetic.xcplaygroundpage/Contents.swift
+++ Lab - Operators.playground/Pages/1. Exercise - Basic Arithmetic.xcplaygroundpage/Contents.swift
@@ -3,14 +3,18 @@
  
  You decide to build a shed and want to know beforehand the area of your yard that it will take up. Create two constants, `width` and `height`, with values of 10 and 20, respectively. Create an `area` constant that is the result of multiplying the two previous constants together, and print out the result.
  */
-let width = 2
-let length = 4
-print(width * length)
+// SU edits
+let width = 2 // let width = 10
+let length = 4 // let height = 20
+// let area = width * height
+print(width * length) // print(area)
+
 /*:
  You decide that you'll divide your shed into two rooms. You want to know if dividing it equally will leave enough room for some of your larger storage items. Create a `roomArea` constant that is the result of dividing `area` in half. Print out the result.
  */
-let area = width * length
+let area = width * length // This code was not needed.
 let roomArea = area / 2
+//print(roomArea)
 
 
 /*:
@@ -21,14 +25,16 @@ print(perimeter)
 /*:
  Print what you would expect the result of integer division of 10 divided by 3 to be. Create a constant, `integerDivisionResult` that is the result of 10 divided by 3, and print the value.
  */
-let integerDivisionResult: Double = 10
-print(integerDivisionResult / 3)
+//print(3)
+let integerDivisionResult: Double = 10 // let integerDivisionResult = 10 / 3
+print(integerDivisionResult / 3) // print(integerDivisionResult)
 /*:
  Now create two constants, `double10` and `double3`, set to 10 and 3, and declare their types as `Double` values. Declare a final constant `divisionResult` equal to the result of `double10` divided by `double3`. Print the value of `divisionResult`. How does this differ from the value when using integer division?
  */
 let double10: Double = 10
 let double3: Double = 3
 let divisionResult = double10 / double3
+// print(divisionResult)
 /*:
  Given the value pi (3.1415927), create a `radius` constant with a value of 5.0, then calculate the diameter and circumference of the circle using the following equations, and print the results:
  
@@ -47,6 +53,7 @@ let y = 53
 
 let radius = 5.0
 let diameter = 2 * radius
-let circumference = pi * diameter
-print("The diameter of the circle is \(diameter) and the circumference is \(circumference)")
+let circumference = pi * diameter // let circumference = 2 * pi * radius
+print("The diameter of the circle is \(diameter) and the circumference is \(circumference)") // print(diameter)
+// print(circumference)
 //: page 1 of 8  |  [Next: App Exercise - Fitness Calculations](@next)
